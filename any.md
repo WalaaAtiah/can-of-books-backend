b04a6d63fdf0cff784d399b58f530ee0eba187c2
WalaaAtiah <walaaatiah97@gmail.com>
2022-09-04 23:17:16 +0300

    first-class11

---
+++ any.md
@@ -0,0 +1,34 @@
+
+
+
+## Logistical
+### 1- What hours will you be available to communicate?
+
+### 2- What platform will you use to communicate (ie. Slack, phone ...)?
+
+### 3- How often will you take breaks?
+
+### 4- What is your plan if you start to fall behind?
+
+<br>
+<br>
+
+## Cooperative
+### 1- Make a list of each parson's strengths.
+
+### 2- How can you best utilize these strengths in the development of your application?
+
+### 3- In what areas do you each want to develop greater strength?
+
+### 4- Knowing that every person in your team needs to understand the code, how do you plan to approach the day-to-day development?
+
+
+<br>
+<br>
+
+## Conflict Resolution
+### 1- What will your team do if one person is pulling all the weight while the other person is not contributing?
+
+### 2- What will your team do if one person is taking over the project and not letting the other member contribute?
+
+### 3- How will you approach each other and the challenge of building an application knowing that it is impossible for two people to be at the exact same place in understanding and skill level?
--- server.js
+++ server.js
@@ -8,11 +8,72 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+const mongoose = require('mongoose'); // 0 - import mongoose
 
+// mongoose config
+mongoose.connect('mongodb://localhost:27017/Books', {useNewUrlParser: true, useUnifiedTopology: true}); // 1 - connect mongoose with DB (books)
+
+const Bookschema = new mongoose.Schema({ //define the schema (structure)
+  title: String,
+  description: String,
+  status: String
+
+});
+const Bookmodel = mongoose.model('book', Bookschema); //compile the schem into a model
+
+
+//seed data (insert initial data)
+async function seedData(){
+  const firstBook = new Bookmodel({
+    title: "Do Androids Dream of Electric Sheep",
+    description: "It was January 2021, and Rick Deckard had a license to kill.Somewhere among the hordes of humans out there, lurked several rogue androids. Deckard's assignment--find them and then...'retire' them. Trouble was, the androids all looked exactly like humans, and they didn't want to be found",
+    status: "Available"
+  })
+
+  const secondBook = new Bookmodel({
+    title: "Moloka'i",
+    description: "Set in Hawai'i more than a century ago, this is the story of Rachel Kalama, a spirited seven-year-old Hawaiian girl, who dreams of visiting far-off lands like her father, a merchant seaman. Then one day a rose-colored mark appears on her skin, and those dreams are stolen from her. Taken from her home and family, Rachel is sent to Kalaupapa, the quarantined leprosy settleme",
+    status: "Available",
+  })
+
+  const thirdBook = new Bookmodel({
+    title: "The Secret Garden",
+    description: "After the death of her parents, Mary Lennox is brought back from India as a forlorn and unwanted child to live in her uncle's great lonely house on the moors. Then one day, she discovers the key to a secret garden and, as if by magic, her life begins to change.",
+    status: "Available"
+  })
+
+  await firstBook.save();
+  await secondBook.save();
+  await thirdBook.save();
+}
+
+
+// seedData(); //call seedData function once
+
+// http://localhost:3001/test
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
+// http://localhost:3001/getbooks
+
+app.get('/getbooks',getbooksHandler);
+
+function getbooksHandler(req,res) {
+
+  Bookmodel.find({},(err,result)=>{
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log(result);
+        res.send(result);
+    }
+  })
+}
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
